a9484f9bc25d6195363899bffc95820371d101f3
Rikelmy Lopes <rikelmylopes899@gmail.com>
2023-09-29 14:19:48 -0300

    style: added electron logo

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import electronLogo from './assets/electron.svg';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
@@ -9,6 +10,9 @@ function App() {
     return (
         <>
             <div>
+                <a href="https://electronjs.org" target="_blank" rel="noreferrer">
+                    <img src={electronLogo} className="logo electron" alt="Electron logo" />
+                </a>
                 <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
                     <img src={viteLogo} className="logo" alt="Vite logo" />
                 </a>
@@ -16,7 +20,7 @@ function App() {
                     <img src={reactLogo} className="logo react" alt="React logo" />
                 </a>
             </div>
-            <h1 >Electron + Vite + React = <span style={{ color: 'red'}} >&#10084;</span></h1>
+            <h1 >Electron + Vite + React = <span className='heart' >&#10084;</span></h1>
             <div className="card">
                 <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -26,7 +30,7 @@ function App() {
                 </p>
             </div>
             <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Electron, Vite and React logos to learn more
             </p>
         </>
     );
